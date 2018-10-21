03f6f012af582ec985befd690cf5a48e8b900543
Dominic Panarello <dom.panarello+public@gmail.com>
2018-10-21 13:14:51 -0600

    Fix(node-web-server): minor refactoring #73

--- app/node-web-server/server.js
+++ app/node-web-server/server.js
@@ -1,3 +1,7 @@
+/* Directories for file uploading and processing */
+const DIR_PROCESSED_FILES = "C:/workdir/processed/";
+const DIR_UPLOADED_FILES = "C:/workdir/upload/";
+
 const express = require('express');
 var app = express();
 var formidable = require('formidable');
@@ -5,6 +9,7 @@ var fs = require('fs');
 if(typeof require !== 'undefined') XLSX = require('xlsx');
 
  
+/* A temporary helper service for testing file upload. */
 app.get('/sourceupload', (req, res) => {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('<form action="source" method="post" enctype="multipart/form-data">');
@@ -14,13 +19,14 @@ app.get('/sourceupload', (req, res) => {
     return res.end();
 });
 
+/* Retrieve the list of known external data source files. */
 app.get('/source', (req, res) => {
     res.writeHead(200, {'Content-Type': 'text/json'});
-    fs.readdir("C:/workdir/processed/", function(err, items) {
+    fs.readdir(DIR_PROCESSED_FILES, function(err, items) {
         console.log(Array.isArray(items));
       
         let statResults = items.map( item => {
-            var myfile = "C:/workdir/processed/" + item;
+            var myfile = DIR_PROCESSED_FILES + item;
             console.log('looking at : ' + myfile);
             var stats = fs.statSync(myfile);
             var result = {};
@@ -34,28 +40,39 @@ app.get('/source', (req, res) => {
     });
 });
 
+/* Upload a new external data source XSLX, and process it. */
 app.post('/source', (req, res) => {
     var form = new formidable.IncomingForm();
+
     form.parse(req, function (err, fields, files) {
       var oldpath = files.filetoupload.path;
-      var newpath = 'C:/workdir/upload/' + files.filetoupload.name;
+      var newpath = DIR_UPLOADED_FILES + files.filetoupload.name;
+      console.log("newpath: "+ newpath);
+      if(!newpath || !newpath.match(/\.(xlsx)$/i)) {
+        res.write('Unsupported file type - must be xslx');
+        res.end();
+        return;
+      }
+          
       fs.rename(oldpath, newpath, function (err) {
-        if (err) throw err;
-        res.write('File uploaded and moved!');
-        var workbook = XLSX.readFile(newpath);
-        var sheet_name_list = workbook.SheetNames;
-        sheet_name_list.forEach(function(y) { /* iterate through sheets */
-            console.log("Processing sheet name : " + y);
-            var worksheet = workbook.Sheets[y];
-            var csv = XLSX.utils.sheet_to_csv(worksheet)
-            fs.writeFile('C:/workdir/processed/' + y + ".csv", csv, (err) => {  
-                // throws an error, you could also catch it here
-                if (err) throw err;
-            
-                // success case, the file was saved
-                console.log('Saved csv: ' + y);
+        res.write('File upload accepted for processing.');
+        if (!err) {
+            var workbook = XLSX.readFile(newpath);
+            var sheet_name_list = workbook.SheetNames;
+            sheet_name_list.forEach(function(y) {
+                var worksheet = workbook.Sheets[y];
+                var csv = XLSX.utils.sheet_to_csv(worksheet)
+                fs.writeFile(DIR_PROCESSED_FILES + y + ".csv", csv, (err) => {  
+                    if (err) throw err;
+                
+                    // success case, the file was saved
+                    console.log('Processed sheet into saved csv: ' + y);
+                });
             });
-        });
+        } else {
+            console.log(err);
+            res.write(err);
+        }
         res.end();
       });
     });
