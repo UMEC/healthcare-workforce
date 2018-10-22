c94fd6fc590f6240a450cd43e81a47f47dcee699
Dominic Panarello <dom.panarello+public@gmail.com>
2018-10-22 15:12:20 -0600

    feat(node-web-server): user session api
    #75

--- app/node-web-server/server.js
+++ app/node-web-server/server.js
@@ -5,5 +5,6 @@ var app = express();
 // Use Api routes in the App
 app.use('/api/source', require("./api/source"))
 app.use('/api/analytics', require("./api/analytics"))
+app.use('/api/user', require("./api/user"))
 
 app.listen(process.env.PORT || 3000)
