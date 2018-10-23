c202abb41f24737b262fe73e2cb2b2557e1a5d1b
Hyuna Yang <hyunayang@gmail.com>
2018-10-23 17:15:24 -0600

    new code

--- models/test/pythonCode/IdealAllocation.py
+++ models/test/pythonCode/IdealAllocation.py
@@ -6,20 +6,13 @@ Created on Mon Oct 22 16:36:52 2018
 @author: yanghy@us.ibm.com
 """
 
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Oct 21 15:16:58 2018
-
-@author: yanghy@us.ibm.com
-"""
 import os
 os.chdir('/Users/yanghy@us.ibm.com/Desktop/test')
 import pandas as pd
 from pulp import *
 import numpy as np
 import sys
-
+FTE_time = 60*8*365
 
 def check_input(a):
     '''
@@ -31,13 +24,16 @@ def check_input(a):
     ser_prov = "ser_prov_ratio.txt"
     ser_demand = "ser_demand_new.txt"
     ser_sutable = "ser_sutab.txt"
-    
+    supply = "supply.txt"
 
     ser_prov = pd.read_csv(ser_prov, sep='\t', encoding = "ISO-8859-1")
     ser_time = pd.read_csv(ser_time, sep='\t', encoding = "ISO-8859-1")
     ser_demand = pd.read_csv(ser_demand, sep='\t', encoding = "ISO-8859-1")
     ser_sutb = pd.read_csv(ser_sutable, sep='\t', encoding = "ISO-8859-1")
+    supply = pd.read_csv(supply, sep='\t', encoding = "ISO-8859-1")
     provider = list(ser_prov)[4:ser_prov.shape[1]]
+    supply = supply[provider]
+    wage = [1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.45, 0.4, 0.3, 0.2, 0.1]
     
     # check size
     print(ser_prov.shape); print(ser_time.shape); print(ser_demand.shape); print(ser_sutb.shape); 
@@ -66,6 +62,8 @@ def check_input(a):
     ser_demand = ser_demand.iloc[p[0], :].reset_index()
     ser_sutb = ser_sutb.iloc[p[0], :].reset_index()
     
+    row_i = range(len(ser_prov))
+    col_j = range(len(provider))
     #====== check float- expand columns 
     kk=[]
     for x in ser_prov['Physician']:
@@ -110,7 +108,7 @@ def what_if_input_process():
     '''
     
     
-def call_opt(w_weight, s_weight, sutability, total_demand, ser_max, p_min, row_i, col_j, wage, provider ):
+def call_opt_ideal(w_weight, s_weight, sutability, total_demand, ser_max, p_min, row_i, col_j, wage, provider ):
     '''
     core LP to optimize the allocation by wage or priority
     '''
@@ -133,7 +131,7 @@ def call_opt(w_weight, s_weight, sutability, total_demand, ser_max, p_min, row_i
             #    prob += service[i][j] <= total_demand.iloc[i,0] + total_demand.iloc[i,1]
             #else: 
             prob += service[(i,j)] >= 0
-            prob += service[(i,j)] <= ser_max.iloc[i,j]/2, ""
+            prob += service[(i,j)] <= ser_max.iloc[i,j], ""
                  
     GLPK().solve(prob)
     # Solution
@@ -148,19 +146,65 @@ def call_opt(w_weight, s_weight, sutability, total_demand, ser_max, p_min, row_i
         m = m.split(',')
         dataset.iloc[ int(m[0]), int(m[1]) ] = v.varValue
         tt = tt + v.varValue       
-    dataset.columns = provider     
-    if(tt == 0 ): 
-        print('error')
-        sys.exit()    
-    return dataset
+    dataset.columns = provider       
+    return dataset, tt
+
+def call_opt_current(w_weight,s_weight,sutability,total_demand,ser_max,p_min,row_i,col_j,wage,provider,supply):
+    '''
+    core LP to optimize the allocation by wage or priority
+    '''
+    supply_check = ser_max.apply(sum, axis = 0)/FTE_time < supply
+    if(   (~supply_check).all(axis = 1)[0]    ):
+        prob = LpProblem("test1", LpMinimize)
+        service = LpVariable.dicts("service", ((i, j) for i in row_i for j in col_j ), lowBound = 0)
     
-def ideal_allocation( ser_time, ser_prov, ser_demand, ser_sutb, provider, wage, w_weight, s_weight , provider ):
+        prob += lpSum([lpSum([service[(i, j)]*(wage[j]*w_weight + sutability.iloc[i,j]*s_weight) for j in col_j ]) for i in row_i ] )
+        #prob += lpSum( [ lpSum([service[(i, j)]* wage[j] for j in col_j ]) for i in row_i ] )
+        # constrains
+        for i in row_i:
+            prob += lpSum([service[(i,j)] for j in col_j]) == total_demand.iloc[i,0], "" # + total_demand.iloc[i,1] )
+            
+        for j in col_j:
+            if( (supply[provider[j]] > 0)[0] ):
+                prob += lpSum([service[(i,j)] for i in row_i]) >= supply[provider[j]]*FTE_time, "" # + total_demand.iloc[i,1] )
+        #========== boundary 
+        for i in row_i:
+            for j in col_j:
+                #prob += service[i][j] <= total_demand.iloc[i,0]
+                #if( j == 0 ): 
+                #    prob += service[i][j] >= total_demand.iloc[i,1]
+                #    prob += service[i][j] <= total_demand.iloc[i,0] + total_demand.iloc[i,1]
+                #else: 
+                prob += service[(i,j)] >= 0
+                prob += service[(i,j)] <= ser_max.iloc[i,j], ""
+                
+                     
+        GLPK().solve(prob)
+        # Solution
+        
+        dataset = pd.DataFrame(np.nan, row_i, col_j); 
+        o = []; tt = 0 
+        for v in prob.variables():
+            m = v.name 
+            m =  m.replace( 'service_(' , "")
+            m =  m.replace( '_' , "")
+            m =  m.replace( ')' , "")
+            m = m.split(',')
+            dataset.iloc[ int(m[0]), int(m[1]) ] = v.varValue
+            tt = tt + v.varValue       
+        dataset.columns = provider     
+    else: 
+        dataset = np.array(provider)[ supply_check.values[0]  ]
+        print("Excess providers in:",end=" "); print(','.join(dataset) )
+        tt = 0
+    return dataset, tt
+
+    
+def resource_allocation(Prov_comp_type, ser_prov, ser_demand, ser_sutb, provider, wage, w_weight, s_weight, provider):
     '''
     description
     '''
     # dimension
-    
-    wage = [1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.45, 0.4, 0.3, 0.2, 0.1]
     
     n_ser = len(ser_demand)
     n_provider = len(provider)
@@ -176,7 +220,7 @@ def ideal_allocation( ser_time, ser_prov, ser_demand, ser_sutb, provider, wage, 
         total_demand.iloc[k,1]= ser_demand.loc[k,'Demand']*  \
                 (ser_time.loc[k,'Physician'])*ser_time.loc[k,'freq']
               
-    #========== boundary === we may not need this
+    
     ser_max = pd.DataFrame(index=range(n_ser),columns=provider)
     p_min = pd.DataFrame(index=range(n_ser),columns=['min_phy'])
     
@@ -191,16 +235,53 @@ def ideal_allocation( ser_time, ser_prov, ser_demand, ser_sutb, provider, wage, 
                     
     #====== optimization
     total_wage = []; total_sutab = []; d = pd.DataFrame(index = provider)
-    for i in np.arange(0, 1, 0.1):
+    for i in np.arange(0, 1.1, 0.1):
         w_weight = i; s_weight = 1- i
-        dataset = call_opt(w_weight, s_weight, sutability, total_demand,  ser_max, p_min, row_i, col_j, wage , provider)
-        
+        if( Prov_comp_type == 'ideal'):
+            dataset, tt = call_opt_ideal(w_weight, s_weight, sutability, total_demand,ser_max, p_min, row_i, col_j, wage , provider)
+        if( Prov_comp_type == 'current'):
+            dataset, tt = call_opt_current(w_weight,s_weight,sutability,total_demand,ser_max,p_min,row_i,col_j,wage,provider,supply)
         # calculate statistics
-        demand = dataset.apply(sum, axis = 0)/(60*8*365)
-        d = pd.concat( [d, pd.DataFrame(data = demand) ], axis = 1) 
-        total_wage.append( np.round( sum(demand*wage), 0) )
-        total_sutab.append( sum((dataset * sutability).apply(sum, axis = 0))/sum(dataset.apply(sum, axis = 0)) )
+        if tt == 0:
+            df = pd.DataFrame(np.nan, index=provider, columns = [i])
+            d = pd.concat( [d, df], axis = 1) 
+            total_wage.append( np.nan )
+            total_sutab.append( np.nan )
+        else:
+            demand = dataset.apply(sum, axis = 0)/FTE_time
+            d = pd.concat( [d, pd.DataFrame(data = demand, columns = [i]) ], axis = 1) 
+            total_wage.append( np.round( sum(demand*wage), 0) )
+            total_sutab.append( sum((dataset * sutability).apply(sum, axis = 0))/sum(dataset.apply(sum, axis = 0)) )
+    
+    d = round( d*10/5 )/2
     return total_tage, total_sutab, d        
+
+def plotthis():
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    # Create some mock data
+    t = np.arange(0, 1.1, 0.1)
+    data1 = np.exp(t)
+    data2 = np.sin(2 * np.pi * t)
+
+    fig, ax1 = plt.subplots()
+
+    color = 'tab:red'
+    ax1.set_xlabel('Ratio betewwn Wage and Priority')
+    ax1.set_ylabel('Wage', color=color)
+    ax1.plot(t, total_wage, color=color)
+    ax1.tick_params(axis='y', labelcolor=color)
+
+    ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+
+    color = 'tab:blue'
+    ax2.set_ylabel('Priority', color=color)  # we already handled the x-label with ax1
+    ax2.plot(t, total_sutab, color=color)
+    ax2.tick_params(axis='y', labelcolor=color)
+
+    fig.tight_layout()  # otherwise the right y-label is slightly clipped
+    plt.show()    
     
 if __name__ == '__main__': 
     pass
