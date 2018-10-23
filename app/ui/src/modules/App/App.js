670c745b786d3bcfa5d5016e0aa9f8e7ff8a275e
Dom Washburn <domwashburn@us.ibm.com>
2018-10-23 09:58:44 -0600

    chore: remove react app boilerplate

--- app/ui/src/modules/App/App.js
+++ app/ui/src/modules/App/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import logo from '../../assets/images/logo.svg';
 import Home from '../Home';
 import Admin from '../Admin';
 import './App.scss';
@@ -10,20 +9,6 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
           <MainNavigation />
           <Route exact path="/" component={Home} />
           <Route exact path="/admin" component={Admin} />
