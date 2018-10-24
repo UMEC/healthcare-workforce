64d7a0eca58f2a2ac6fe5fa3fdd07bfbb61414e9
Dominic Panarello <dom.panarello+public@gmail.com>
2018-10-24 15:41:32 -0600

    fix(ui): tested node-react integration
    #28

--- ui/src/modules/App/App.js
+++ ui/src/modules/App/App.js
@@ -5,6 +5,28 @@ import Admin from '../Admin';
 import './App.scss';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      response: '',
+    }
+  }
+  
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res[0].name }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/source');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+    console.log("Received body: " + body);
+    return body;
+  };
+  
   render() {
     return (
       <Router>
@@ -12,6 +34,7 @@ class App extends Component {
           <MainNavigation />
           <Route exact path="/" component={Home} />
           <Route exact path="/admin" component={Admin} />
+          <p className="App-intro">{this.state.response}</p>
         </div>
       </Router>
     );
