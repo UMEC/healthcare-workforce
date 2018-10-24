263472cd95d2913cdc947dea62329edbfcb51862
Dom Washburn <domwashburn@us.ibm.com>
2018-10-24 16:29:25 -0600

    chore: move new components from old UI dir to new dir
    Moving the UI shell components to the new location of the UI and  removing the unsed user info component

--- ui/src/modules/Home/Home.js
+++ ui/src/modules/Home/Home.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import UserInfo from '../../components/UserInfo';
 
 class Home extends Component {
   render() {
     return (
       <>
         <p>Home</p>
-        <UserInfo>{this.props.user.name}</UserInfo>
       </>
     );
   }
