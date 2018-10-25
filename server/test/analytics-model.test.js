ffbf7119aa9851791ec1229bcfacc453cfe80920
Dominic Panarello <dom.panarello+public@gmail.com>
2018-10-25 16:03:47 -0600

    fix(python): unit test error
    #118

--- server/test/analytics-model.test.js
+++ server/test/analytics-model.test.js
@@ -9,8 +9,9 @@ describe('Test the analytics model requests', () => {
   test('Check valid response model request', (done) => {
     analyticsModel.invokeModelRequest({ request_type: 'provider_profile', value: 'Psych' }, (err, responseJson) => {
       if (err) {
-        console.log(err);
+        console.log("damn, found error: " + err);
         console.log(JSON.stringify(responseJson));
+        throw new Error(err);
       }
       expect(responseJson.response[0].header[0].provider_name).toBe('Psychiatrist');
       done();
