0b566b5d6a1d3f6cdfdc96065f63656151e9d032
Dominic Panarello <dom.panarello+public@gmail.com>
2018-10-25 15:00:13 -0600

    fix(server): updated python integration and unit tests
    #138

---
+++ server/test/analytics-model.test.js
@@ -0,0 +1,22 @@
+/*
+ * A series of tests for the analytics model service.
+ * Some of these requests require session context,
+ * to mock an application user making consecutive requests.
+ */
+const analyticsModel = require('../service/analytics-model');
+
+describe('Test the analytics model requests', () => {
+  test('Check valid response model request', (done) => {
+    analyticsModel.invokeModelRequest({ request_type: 'provider_profile', value: 'Psych' }, (err, responseJson) => {
+      expect(responseJson.response[0].header[0].provider_name).toBe('Psychiatrist');
+      done();
+    });
+  });
+
+  test('Check valid response model request for invalid arguments', (done) => {
+    analyticsModel.invokeModelRequest({ myparam1: 'myvalue1' }, (err, responseJson) => {
+      expect(responseJson.response[0].error_msg).toBe('ERROR: Could not parse argument: \'value\'');
+      done();
+    });
+  });
+});
