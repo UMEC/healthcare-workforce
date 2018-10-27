333ca6120568d89413f58ac57461fbecf001c97e
Dom Washburn <domwashburn@us.ibm.com>
2018-10-26 23:35:26 -0600

    refactor: remove dead code from saga

--- ui/src/sagas/index.js
+++ ui/src/sagas/index.js
@@ -12,35 +12,6 @@ import { all, call, put, takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
 import { loadStateFromSessionStorage } from '../loadState';
 
-// function* postAnalytics() {
-//   const response = fetch('/api/analytics', {
-//     method: 'POST',
-//     headers: {
-//       'Accept': 'application/json',
-//       'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify({ "params": { "type": "cost_quality_adjustment", "value": 0.5 } })
-//   });
-
-//   // if (response.status !== 200) throw Error(body.message);
-//   // console.log("Post respnse: " + JSON.stringify(body));
-//   return response.json();
-// };
-
-// function* callAnalyticsGetApi(modelId) {
-//   const response = fetch('/api/analytics/' + modelId, {
-//     method: 'GET',
-//     headers: {
-//       'Accept': 'application/json',
-//       'Content-Type': 'application/json',
-//     }
-//   });
-
-//   // if (response.status !== 200) throw Error(body.message);
-//   // console.log("Get response: " + JSON.stringify(body));
-//   return response.json();
-// };
-
 const headers = {
   'Accept': 'application/json',
   'Content-Type': 'application/json',
@@ -65,30 +36,9 @@ const getModelData = (modelId) => {
     .then(response => ({ response }))
     .catch(error => ({ error }));
 }
-
-// const callAnalyticsGetApi = async (modelId) => {
-//   const response = await fetch('/api/analytics/' + modelId, {
-//     method: 'GET',
-//     headers: {
-//       'Accept': 'application/json',
-//       'Content-Type': 'application/json',
-//     }
-//   });
-
-//   const body = await response.json();
-
-//   if (response.status !== 200) throw Error(body.message);
-//   console.log("Get response: " + JSON.stringify(body));
-//   return body;
-// };
-
 function* requestModelInfo(modelParams) {
   let { response, error } = yield call(getModelInfo, modelParams);
-  // const body = yield response;
-  // let {response2, error2} = yield call(getModelData, response.data.modelId);
-  // debugger;
 
-  
   if (response) {
     let { modelId } = response.data;
 
@@ -98,8 +48,6 @@ function* requestModelInfo(modelParams) {
   } else {
     yield put({ type: MODEL_INFO_FAILURE, error })
   } 
-  // let foo = yield call()
-  
 }
 
 function* requestModel(modelId) {
@@ -108,7 +56,7 @@ function* requestModel(modelId) {
   if (response) {
     yield put({ type: MODEL_SUCCESS, payload: response.data })
   } else {
-    yield put({ type: MODEL_FAILURE, payload: error })
+    yield put({ type: MODEL_FAILURE, payload: error.response })
   }
 }
 
