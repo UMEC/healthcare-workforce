40d1ea0ac13a20666239bc6d295fbd29242677b6
Dom Washburn <domwashburn@us.ibm.com>
2018-10-28 13:59:11 -0600

    chore: temporarily set activeModel default state to static object

--- ui/src/reducers/reducer-model.js
+++ ui/src/reducers/reducer-model.js
@@ -1,3 +1,5 @@
+import activeModelDefaultState from './mock-default-state';
+import _ from 'lodash';
 import {
   MODEL_REQUEST,
   MODEL_SUCCESS,
@@ -5,14 +7,20 @@ import {
 } from '../actions';
 // import { actionChannel } from 'redux-saga/effects';
 
-const initialState = {};
+// reshaping default state to next the 
+const provider = activeModelDefaultState.response.provider;
+const supply = _.mapValues(_.groupBy(activeModelDefaultState.response.supply, 'provider_county'));
+const services = _.mapValues(_.groupBy(activeModelDefaultState.response.services, 'acute_encounter'));
 
-export default (state = initialState, action) => {
+debugger;
+
+export default (state = {}, action) => {
   switch (action.type) {
     case MODEL_REQUEST:
       return { loading: true, error: null };
     case MODEL_SUCCESS:
-      return action.payload;
+      // return action.payload;
+      return { defaultResponse: activeModelDefaultState, provider, supply, services };
     case MODEL_FAILURE:
       return action.payload;
     default:
