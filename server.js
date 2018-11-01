fa7a150b9bb67bda777f958db7ae3701b299c0de
Dominic Panarello <dom.panarello+public@gmail.com>
2018-10-31 18:40:34 -0600

    feat(server): added swagger doco into repo

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ if (cluster.isMaster && process.env.NODE_ENV !== 'unittest') {
   app.use('/api/source', require("./server/api/source"))
   app.use('/api/analytics', require("./server/api/analytics"))
   app.use('/api/user', require("./server/api/user"))
-  app.use('/api/admin', require("./server/ui/admin"))
+  app.use('/api', require("./server/ui/index"))
 
   if (process.env.NODE_ENV !== 'unittest') {
     app.listen(process.env.PORT || 5000)
