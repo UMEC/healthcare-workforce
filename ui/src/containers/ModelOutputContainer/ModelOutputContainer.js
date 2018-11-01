b15e76ab6e6745cce2125436c2e225541bea1f51
Dominic Panarello <dom.panarello+public@gmail.com>
2018-10-31 18:11:55 -0600

    feat(ui): added county title

--- ui/src/containers/ModelOutputContainer/ModelOutputContainer.js
+++ ui/src/containers/ModelOutputContainer/ModelOutputContainer.js
@@ -62,6 +62,15 @@ class ModelOutputContainer extends Component {
 
     return (
       <>
+          <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+            <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">UMEC Gap Analysis Tool</a>
+            <a class="navbar-brand col-sm-3 col-md-2 mr-0 header-county-title" href="#">{this.props.modelFilters.activeFilters.geo.area}</a>
+            <ul class="navbar-nav px-3">
+              <li class="nav-item text-nowrap">
+                <a class="nav-link" href="/">Home</a>
+              </li>
+            </ul>
+          </nav>
           <div class="container-fluid">
             <div class="row">
               <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
